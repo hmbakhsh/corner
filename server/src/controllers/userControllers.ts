557f00d9c119fbf183cf735b60332ad4dcebf386
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-07-25 15:50:58 +0100

    feat(models): implement user controllers

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -1,11 +1,38 @@
 import type { Request, Response } from "express";
-import { getUsersModel } from "../models/userModels";
+import {
+	createUserModel,
+	getUserModel,
+	getUsersModel,
+} from "../models/userModels";
 
-export const getUsersController = (req: Request, res: Response) => {
+// Controller: Get All Users
+export const getUsersController = async (req: Request, res: Response) => {
 	try {
-		const users = getUsersModel();
+		const users = await getUsersModel();
 		res.send(users).status(200);
 	} catch (err) {
 		console.log(err);
+	}
+};
+
+// Controller: Create User
+export const createUserController = async (req: Request, res: Response) => {
+	try {
+		const { username, email, password_hash } = req.body;
+		const createdUser = await createUserModel(username, email, password_hash);
+		res.status(201).send(createdUser);
+	} catch (err) {
+		console.log(`Failed: Create User Controller: ${err}`);
+	}
+};
+
+// Controller: Get User
+export const getUserController = async (req: Request, res: Response) => {
+	try {
+		const { userId } = req.params;
+		const user = await getUserModel(Number.parseInt(userId));
+		res.status(200).send(user);
+	} catch (err) {
+		console.log(`Failed: Get User Controller ${err}`);
 	}
 };
