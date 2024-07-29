91d57e6a0352c921547c0392ac6a730ca89394cd
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-07-29 12:12:42 +0100

    feat(models): implement models for fetching all primitive data

--- server/src/models/primitiveModels.ts
+++ server/src/models/primitiveModels.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "..";
 import {
 	embedPrimitives,
@@ -16,20 +16,77 @@ import {
 } from "./schema";
 import type { updateObjectType } from "../controllers/primitiveControllers";
 
+// Get All Primitives for a User's Corner
+// Params: User_Id, Corner_Id
 export const getUserCornerPrimitivesModel = async (
 	user_id: number,
 	corner_id: number,
 ) => {
-	return await db
+	const cornerTextPrimitives = await db
 		.select()
 		.from(primitives)
-		.where(eq(primitives.corner_id, corner_id))
+		.where(
+			and(
+				eq(primitives.corner_id, corner_id),
+				eq(primitives.primitive_type, "text"),
+			),
+		)
 		.leftJoin(textPrimitives, eq(primitives.id, textPrimitives.primitive_id));
+
+	const cornerShapePrimitives = await db
+		.select()
+		.from(primitives)
+		.where(
+			and(
+				eq(primitives.corner_id, corner_id),
+				eq(primitives.primitive_type, "shape"),
+			),
+		)
+		.leftJoin(shapePrimitives, eq(primitives.id, shapePrimitives.primitive_id));
+	const cornerImagePrimitives = await db
+		.select()
+		.from(primitives)
+		.where(
+			and(
+				eq(primitives.corner_id, corner_id),
+				eq(primitives.primitive_type, "image"),
+			),
+		)
+		.leftJoin(imagePrimitives, eq(primitives.id, imagePrimitives.primitive_id));
+
+	const cornerLinkPrimitives = await db
+		.select()
+		.from(primitives)
+		.where(
+			and(
+				eq(primitives.corner_id, corner_id),
+				eq(primitives.primitive_type, "link"),
+			),
+		)
+		.leftJoin(linkPrimitives, eq(primitives.id, linkPrimitives.primitive_id));
+
+	const cornerEmbedPrimitives = await db
+		.select()
+		.from(primitives)
+		.where(
+			and(
+				eq(primitives.corner_id, corner_id),
+				eq(primitives.primitive_type, "embed"),
+			),
+		)
+		.leftJoin(embedPrimitives, eq(primitives.id, embedPrimitives.primitive_id));
+
+	return [
+		...cornerTextPrimitives,
+		...cornerShapePrimitives,
+		...cornerImagePrimitives,
+		...cornerLinkPrimitives,
+		...cornerEmbedPrimitives,
+	];
 };
 
 export const createPrimitiveModel = async (
 	corner_id: string,
-	// primitive_type: primitiveTypeType,
 	width: number,
 	height: number,
 	primitiveSpecificData:
@@ -52,40 +109,48 @@ export const createPrimitiveModel = async (
 				.returning();
 
 			switch (primitiveSpecificData.type) {
-				case "text":
-					await trx.insert(textPrimitives).values({
-						primitive_id: newPrimitive.id,
-						text_content: primitiveSpecificData.text_content,
-						text_colour: primitiveSpecificData.text_colour,
-					});
+				case "text": {
+					await trx
+						.insert(textPrimitives)
+						.values({
+							primitive_id: newPrimitive.id,
+							text_content: primitiveSpecificData.text_content,
+							text_colour: primitiveSpecificData.text_colour,
+						})
+						.returning();
 					break;
-				case "shape":
+				}
+				case "shape": {
 					await trx.insert(shapePrimitives).values({
 						primitive_id: newPrimitive.id,
 						shape_type: primitiveSpecificData.shape_type,
 						shape_colour: primitiveSpecificData.shape_colour,
 					});
 					break;
-				case "image":
+				}
+				case "image": {
 					await trx.insert(imagePrimitives).values({
 						primitive_id: newPrimitive.id,
 						image_url: primitiveSpecificData.image_url,
 					});
 					break;
-				case "link":
+				}
+				case "link": {
 					await trx.insert(linkPrimitives).values({
 						primitive_id: newPrimitive.id,
 						link_title: primitiveSpecificData.link_title,
 						link_url: primitiveSpecificData.link_url,
 					});
 					break;
-				case "embed":
+				}
+				case "embed": {
 					await trx.insert(embedPrimitives).values({
 						primitive_id: newPrimitive.id,
 						embed_type: primitiveSpecificData.embed_type,
 						embed_url: primitiveSpecificData.embed_url,
 					});
 					break;
+				}
 				default:
 					throw new Error(
 						"primitiveModels.ts: Passed unsupported primitive type",
@@ -93,7 +158,58 @@ export const createPrimitiveModel = async (
 			}
 			return newPrimitive;
 		});
-		return result;
+
+		switch (result.primitive_type) {
+			case "text": {
+				const newTextPrimitiveData = await db
+					.select()
+					.from(primitives)
+					.where(eq(primitives.id, result.id))
+					.leftJoin(textPrimitives, eq(textPrimitives.primitive_id, result.id));
+				return newTextPrimitiveData[0];
+			}
+			case "shape": {
+				const newShapePrimitiveData = await db
+					.select()
+					.from(primitives)
+					.where(eq(primitives.id, result.id))
+					.leftJoin(
+						shapePrimitives,
+						eq(shapePrimitives.primitive_id, result.id),
+					);
+				return newShapePrimitiveData[0];
+			}
+			case "image": {
+				const newImagePrimitiveData = await db
+					.select()
+					.from(primitives)
+					.where(eq(primitives.id, result.id))
+					.leftJoin(
+						imagePrimitives,
+						eq(imagePrimitives.primitive_id, result.id),
+					);
+				return newImagePrimitiveData[0];
+			}
+			case "link": {
+				const newLinkPrimitiveData = await db
+					.select()
+					.from(primitives)
+					.where(eq(primitives.id, result.id))
+					.leftJoin(linkPrimitives, eq(linkPrimitives.primitive_id, result.id));
+				return newLinkPrimitiveData[0];
+			}
+			case "embed": {
+				const newEmbedPrimitiveData = await db
+					.select()
+					.from(primitives)
+					.where(eq(primitives.id, result.id))
+					.leftJoin(
+						embedPrimitives,
+						eq(embedPrimitives.primitive_id, result.id),
+					);
+				return newEmbedPrimitiveData[0];
+			}
+		}
 	} catch (err) {
 		// !  Improve this error message
 		console.error("Failed to post to the database", err);
